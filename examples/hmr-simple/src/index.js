30bd4ae0aedf70e1ab2192cb46763c08491679d8
GilbertSun <szb4321@gmail.com>
2018-03-04 13:38:36 +0800

    refine hmr-simple demo

--- examples/hmr-simple/src/index.js
+++ examples/hmr-simple/src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
-import printMe from './printMe';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello1', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
@@ -18,7 +18,7 @@ function component() {
 document.body.appendChild(component());
 
 if (module.hot) {
-  module.hot.accept('./printMe', function () {
+  module.hot.accept('./print.js', function () {
     console.log('Accepting the updated printMe module!');
     printMe();
   })
